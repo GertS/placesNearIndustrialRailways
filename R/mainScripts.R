d7034a0bc05ccc0cf4475774ba31d813d198011e
JobdePater <job.depater@wur.nl>
2016-01-11 13:04:12 +0100

    edit

---
+++ R/mainScripts.R
@@ -0,0 +1,9 @@
+# Job de pater 20
+# 160109
+# Assignement 6 GeoScripting
+
+
+# link naar NL places: www.mapcruzin.com/download-shapefile/netherlands-places-shape.zip
+# link naar NL rails: www.mapcruzin.com/download-shapefile/netherlands-railways-shape.zip
+
+download.file(url = 'https://github.com/GeoScripting-WUR/IntroToRaster/raw/gh-pages/data/gewata.zip', destfile = 'gewata.zip', method = 'wget')
